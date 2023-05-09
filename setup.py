3cfbdd2113ea3f838fc7d97e984dd3b2ca0fed9a
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2023-05-08 20:16:09 -0400

    Update codecov integration (#17)
    * Update codecov integration
    
    * Update setup pypandoc
    
    * INstall pandoc and pypandoc

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+import pypandoc
 
 setup(
     name='rpq',
     version='2.2',
     description='Simple Redis work queue with added features (priorities, pop multiple items at once)',
-    long_description=long_description,
+    long_description=pypandoc.convert_file('README.md', 'rst'),
     author='Gabriel Bordeaux',
     author_email='pypi@gab.lc',
     url='https://github.com/gabfl/redis-priority-queue',
